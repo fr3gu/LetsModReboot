0197a9de007062ad2994d9b29ac54800f58bf8c6
Fredrik Olsson <fr3gu77@gmail.com>
2014-11-12 22:41:50 +0100

    Update

--- src/main/java/com/fr3gu/letsmodreboot/command/SampleCommand.java
+++ src/main/java/com/fr3gu/letsmodreboot/command/SampleCommand.java
@@ -4,8 +4,12 @@ import net.minecraft.block.Block;
 import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.dedicated.DedicatedServer;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.world.World;
 
+import javax.swing.text.html.parser.Entity;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,29 +43,66 @@ public class SampleCommand implements ICommand {
 
     @Override
     public void processCommand(ICommandSender commandSender, String[] input) {
-        final EntityPlayer p = (EntityPlayer) commandSender;
+        World world = null;
+        ICommandSender sender = commandSender instanceof EntityPlayer ? ((EntityPlayer) commandSender) : null;
+        if(sender == null) {
+            sender = commandSender instanceof DedicatedServer ? ((DedicatedServer) commandSender) : null;
+            world = ((DedicatedServer)sender).getEntityWorld();
+        }
+        else {
+            world = ((EntityPlayer)sender).worldObj;
+        }
+
+        double playerPosX = 0;
+        double playerPosY = 64;
+        double playerPosZ = 0;
+
+        if(sender instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer)sender;
+            playerPosX = Math.floor(player.posX);
+            playerPosY = Math.floor(player.posY);
+            playerPosZ = Math.floor(player.posZ);
+        }
+
+        if(input.length == 5) {
+            playerPosX = Integer.parseInt(input[3]);
+            playerPosY = Integer.parseInt(input[4]);
+            playerPosZ = Integer.parseInt(input[5]);
+        }
 
         if(input.length == 0) {
-            p.addChatMessage(new ChatComponentText("Inga argument!"));
+            sender.addChatMessage(new ChatComponentText("Inga argument!"));
             return;
         }
 
         if(input[0].equalsIgnoreCase("help")) {
-            p.addChatMessage(new ChatComponentText("Enter block name as unlocalized with modid, i.e. \"letsmodreboot:flag\""));
+            sender.addChatMessage(new ChatComponentText("Enter block name as unlocalized with modid, i.e. \"letsmodreboot:flag\""));
             return;
         }
 
-        try {
+        String blockNameIn = input[0];
+        int radius = 5;
+        int heightRadius = 5;
+        if(input.length == 3) {
+            System.out.println("Radius: " + input[1]);
+            System.out.println("Height radius: " + input[2]);
+            radius = Integer.parseInt(input[1]);
+            heightRadius = Integer.parseInt(input[2]);
+        }
+
+        //try {
             ArrayList<Reka> foundBlocks = new ArrayList<Reka>();
-            for (int z = -5; z < 5; z++) {
-                for (int y = -5; y < 5; y++) {
-                    for (int x = -5; x < 5; x++) {
-                        int posX = (int) p.posX + x;
-                        int posY = (int) p.posY + y;
-                        int posZ = (int) p.posZ + z;
-                        Block b = p.worldObj.getBlock(posX, posY, posZ);
+
+            for (int z = -radius; z < radius; z++) {
+                for (int y = -heightRadius; y < heightRadius; y++) {
+                    for (int x = -radius; x < radius; x++) {
+                        int posX = (int) playerPosX + x;
+                        int posY = (int) playerPosY + y;
+                        int posZ = (int) playerPosZ + z;
+                        //System.out.println("Scanning " + posX + ", " + posY + ", " + posZ);
+                        Block b = world.getBlock(posX, posY, posZ);
                         String blockId = b.getUnlocalizedName();
-                        if(blockId.equals("tile." + input[0])) {
+                        if(blockId.equals("tile." + blockNameIn)) {
                             foundBlocks.add(new Reka(b, posX, posY, posZ));
                         }
                     }
@@ -70,31 +111,31 @@ public class SampleCommand implements ICommand {
 
             if(foundBlocks.size() > 0) {
                 for (Reka r : foundBlocks) {
-                    String blockName = r.getBlock().getLocalizedName();
-                    System.out.println("BlockId: " + blockName);
+                    String foundBlockName = r.getBlock().getLocalizedName();
+                    System.out.println("BlockId: " + foundBlockName);
                     int[] coords = r.getCoords();
-                    p.addChatMessage(new ChatComponentText("Found block (" + blockName + ") at " + coords[0] + " " + coords[1] + " " + coords[2]));
+                    sender.addChatMessage(new ChatComponentText("Found block (" + foundBlockName + ") at " + coords[0] + " " + coords[1] + " " + coords[2]));
                 }
-                p.addChatMessage(new ChatComponentText("---------------------------"));
-                p.addChatMessage(new ChatComponentText("Found blocks: " + foundBlocks.size()));
+                sender.addChatMessage(new ChatComponentText("---------------------------"));
+                sender.addChatMessage(new ChatComponentText("Found blocks: " + foundBlocks.size()));
             }
             else {
-                p.addChatMessage(new ChatComponentText("Could not find block \"" + input[0] + "\""));
+                sender.addChatMessage(new ChatComponentText("Could not find block \"" + blockNameIn + "\""));
             }
 
-            if(input.length > 1 && input.length < 4) {
+            /*if(input.length > 1 && input.length < 4) {
                 int x = Integer.parseInt(input[1]);
                 int y = Integer.parseInt(input[2]);
                 int z = Integer.parseInt(input[3]);
 
-                String blockId = p.worldObj.getBlock(x, y, z).getUnlocalizedName();
-                p.addChatMessage(new ChatComponentText("BlockId (x y z): " + blockId));
-            }
+                String blockId = sender.worldObj.getBlock(x, y, z).getUnlocalizedName();
+                sender.addChatMessage(new ChatComponentText("BlockId (x y z): " + blockId));
+            }*/
 
-        }
-        catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-        }
+        //}
+        //catch (Exception e) {
+        //    System.out.println("Error: " + e.getMessage());
+        //}
 
 
     }
